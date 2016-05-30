141f198200a4ebe99dd90b0f2e4cf4032a86712b
xuwenwen <xuwenwen@xiaomi.com>
2016-05-30 10:49:05 +0800

    up

--- src/main/java/com/leetcode/P16/C169_MajorityElement.java
+++ src/main/java/com/leetcode/P16/C169_MajorityElement.java
@@ -1,9 +1,25 @@
 package com.leetcode.P16;
 
+import java.util.Stack;
+
 /**
  * Created: wwxu(xuwenwen@xiaomi.com)
  * Date: 2016-05-28
  */
 public class C169_MajorityElement {
-    
+    public int majorityElement(int[] nums) {
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i : nums) {
+            if (stack.isEmpty()) {
+                stack.push(i);
+            } else {
+                if (i == stack.peek()) {
+                    stack.push(i);
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.peek();
+    }
 }
